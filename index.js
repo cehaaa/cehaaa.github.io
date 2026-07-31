9e043b6dfad631d48ef49ae628fe9c5825bb6e7b
christian <christiangouw168@gmail.com>
2026-07-31 13:47:36 +0700

    feat: enhance entry links with improved structure and animations

---
+++ index.js
@@ -0,0 +1,24 @@
+const NAMES = ["wanderer", "traveler", "dreamer", "stargazer", "explorer"];
+const GREETINGS = {
+  morning: ["good morning 🌥️", "morning! 🌥️"],
+  afternoon: ["good afternoon ☀️", "afternoon! ☀️"],
+  evening: ["good evening 🌙", "evening! 🌙"],
+};
+
+function random(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function getGreeting() {
+  const hour = new Date().getHours();
+  const template = `hello ${random(NAMES)},`;
+  let greeting = "";
+
+  if (hour < 12) greeting = random(GREETINGS["morning"]);
+  else if (hour < 18) greeting = random(GREETINGS["afternoon"]);
+  else greeting = random(GREETINGS["evening"]);
+
+  return `${template} ${greeting}`;
+}
+
+document.getElementById("greeting").textContent = getGreeting();
